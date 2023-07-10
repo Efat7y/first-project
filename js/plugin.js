f324d01d184165762572c7e2603be32446047bd0
Efat7y <eslamzain8897@gmail.com>
2023-07-10 17:48:55 +0300

    add file Themesn to css and jquery Code

--- js/plugin.js
+++ js/plugin.js
@@ -10,5 +10,26 @@ $(document).ready(function(){
       interval: 3000
       
     });
+    //Show Color Option Div When click On The Gear
+    $('.gear-check').click(function()
+    {
+      $('.color-option').fadeToggle();
+    });
+    // Change Theme Color On Click
+    var colorLi = $('.color-option ul li');
+
+    colorLi
+      .eq(0).css("background-color","#E41B17").end()
+      .eq(1).css("background-color","#61ff6a").end()
+      .eq(2).css("background-color","#59abff").end()
+      .eq(3).css("background-color","#ff39e8").end()
+      .eq(4).css("background-color","#ffa83a").end()
+      .eq(5).css("background-color","#ecf02e");
+
+    colorLi.click(function(){
+      $("link[href*='theme']").attr("href",$(this).attr('data-value'));
+      // console.log($(this).attr('data-value'))
+    });
+
     
 });
